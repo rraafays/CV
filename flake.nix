b1f7a05fd57da242c8a50ae8f85bc0ec3c0bcc92
raf <rraf@tuta.io>
2024-09-07 20:20:43 +0100

    chore: initialised repo

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "cv node package";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOs/nixpkgs";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { nativeBuildInputs = with pkgs; [ nodePackages.pnpm ]; };
+      }
+    );
+}
